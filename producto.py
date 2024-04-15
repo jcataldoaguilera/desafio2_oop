a8054a6ed249cb20cd8defab0ead44a3b45f7412
Juan Cataldo Aguilera <jcataldoaguilera@gmail.com>
2024-04-14 23:26:26 -0400

    entrega producto.py

---
+++ producto.py
@@ -0,0 +1,29 @@
+# Info
+# Autor: Juan Cataldo [jcataldoaguilera@gmail.com]
+# Fecha: 2024-04-10
+# RLAB-23-02-09-0044-2
+#
+
+class Producto():
+    def __init__(self, nombre: str, precio: int, stock:int = 0):
+        self.__nombre = nombre
+        self.__precio = precio
+        self.__stock = stock
+    
+    @property
+    def get_nombre(self):
+        return self.__nombre
+    
+    @property
+    def get_precio(self):
+        return self.__precio
+    
+    @property
+    def get_stock(self):
+        return self.__stock
+    
+    @get_stock.setter
+    def mod_stock(self, cantidad):
+        if cantidad <= 0:
+            cantidad = 0
+        self.__stock += cantidad
